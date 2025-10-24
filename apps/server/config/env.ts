15aca21921571a6451e5853ea985a4d9d0adaaf6
Habib Shaban <h.shaban234@yahoo.com>
2025-10-24 15:40:36 +0300

    feat: add CLIENT_URL to environment configuration

--- apps/server/config/env.ts
+++ apps/server/config/env.ts
@@ -4,12 +4,14 @@ type Env = {
   NODE_ENV: string;
   PORT: number;
   SESSION_SECRET: string;
+  CLIENT_URL: string;
 };
 
 const envObject: Env = {
   NODE_ENV: process.env.NODE_ENV || "development",
   PORT: Number(process.env.PORT) || 3000,
   SESSION_SECRET: process.env.SESSION_SECRET || "",
+  CLIENT_URL: process.env.CLIENT_URL || "http://localhost:5173",
 };
 
 if (!envObject.SESSION_SECRET) {
