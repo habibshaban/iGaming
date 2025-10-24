4103d4d763e2305e896b6e227243f5e2f7d4d0e9
Habib Shaban <h.shaban234@yahoo.com>
2025-10-24 14:24:12 +0300

    feat: add LoginPage component and routing

--- apps/igaming-web/src/routes/index.tsx
+++ apps/igaming-web/src/routes/index.tsx
@@ -2,7 +2,12 @@ import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
 const Home = lazy(() => import("../pages/Home"));
+const LoginPage = lazy(() => import("../pages/LoginPage"));
 export const router = createBrowserRouter([
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
   {
     path: "/",
     element: <Home />,
