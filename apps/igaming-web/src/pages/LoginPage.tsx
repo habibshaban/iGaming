e14151fd7f1f2091253f0b28d925dcc719b48792
Habib Shaban <h.shaban234@yahoo.com>
2025-10-24 15:32:10 +0300

    feat: implement LoginPage

--- apps/igaming-web/src/pages/LoginPage.tsx
+++ apps/igaming-web/src/pages/LoginPage.tsx
@@ -1,5 +1,58 @@
+import { useState } from "react";
+import Input from "../components/Input";
+import Button from "../components/Button";
+import LoginImage from "@/assets/images/logo.png";
+
 const LoginPage = () => {
-  return <div>Login Page</div>;
+  const [isLoading, setIsLoading] = useState(false);
+  const [formData, setFormData] = useState({
+    username: "",
+    password: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    setIsLoading(true);
+    setTimeout(() => setIsLoading(false), 2000);
+  };
+
+  return (
+    <div className="login-page">
+      <div className="login-container">
+        <div className="login-logo">
+          <img src={LoginImage} alt="FinnPlay Logo" width="70" height="70" />
+        </div>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <div className="login-inputs">
+            <Input
+              type="text"
+              label="Username"
+              name="username"
+              value={formData.username}
+              onChange={handleChange}
+            />
+            <Input
+              type="password"
+              label="Password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+            />
+          </div>
+          <Button type="submit" loading={isLoading}>
+            Login
+          </Button>
+        </form>
+      </div>
+    </div>
+  );
 };
 
 export default LoginPage;
