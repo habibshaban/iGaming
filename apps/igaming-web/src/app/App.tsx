01c47709ad7a23c871475f56050c74d54850d034
Habib Shaban <h.shaban234@yahoo.com>
2025-10-24 16:51:20 +0300

    feat: using auth provider

--- apps/igaming-web/src/app/App.tsx
+++ apps/igaming-web/src/app/App.tsx
@@ -1,13 +1,15 @@
 import { Suspense } from "react";
-import { QueryProvider, Router } from "./providers";
+import { AuthProvider, QueryProvider, Router } from "./providers";
 
 export const App = () => {
   return (
     <QueryProvider>
-      {/* TODO: Should Add better UI for loading state */}
-      <Suspense fallback={<div className="app-loading">Loading...</div>}>
-        <Router />
-      </Suspense>
+      <AuthProvider>
+        {/* TODO: Should Add better UI for loading state */}
+        <Suspense fallback={<div className="app-loading">Loading...</div>}>
+          <Router />
+        </Suspense>
+      </AuthProvider>
     </QueryProvider>
   );
 };
