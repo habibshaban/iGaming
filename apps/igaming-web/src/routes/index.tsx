aa0072a7318541e2ddee2decb10dcb975c16e345
Habib Shaban <h.shaban234@yahoo.com>
2025-10-24 14:24:30 +0300

    fix: update import paths in routes to use path alias

--- apps/igaming-web/src/routes/index.tsx
+++ apps/igaming-web/src/routes/index.tsx
@@ -1,8 +1,8 @@
 import { lazy } from "react";
 import { createBrowserRouter, Navigate } from "react-router-dom";
 
-const Home = lazy(() => import("../pages/Home"));
-const LoginPage = lazy(() => import("../pages/LoginPage"));
+const Home = lazy(() => import("@/pages/Home"));
+const LoginPage = lazy(() => import("@/pages/LoginPage"));
 export const router = createBrowserRouter([
   {
     path: "/login",
