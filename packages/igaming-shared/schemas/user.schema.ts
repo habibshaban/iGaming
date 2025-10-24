9d37469b0962e719ffcd6e18a4c5acabd2153466
Habib Shaban <h.shaban234@yahoo.com>
2025-10-24 16:52:43 +0300

    fix: update schema for user pass

--- packages/igaming-shared/schemas/user.schema.ts
+++ packages/igaming-shared/schemas/user.schema.ts
@@ -2,5 +2,5 @@ import { z } from "zod";
 
 export const userSchema = z.object({
   username: z.string().min(3, "Username must be at least 3 characters"),
-  password: z.string().min(8, "Password must be at least 8 characters"),
+  password: z.string().min(4, "Password must be at least 4 characters"),
 });
