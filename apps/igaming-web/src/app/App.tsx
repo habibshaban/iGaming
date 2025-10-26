af3b51b31108f8e4a29a6f8769c31da4c2f1b8f9
Habib Shaban <h.shaban234@yahoo.com>
2025-10-26 07:05:39 +0200

    feat: adding error fallback and suspense fallback

--- apps/igaming-web/src/app/App.tsx
+++ apps/igaming-web/src/app/App.tsx
@@ -1,15 +1,19 @@
 import { Suspense } from "react";
 import { AuthProvider, QueryProvider, Router } from "./providers";
+import { ErrorBoundary } from "react-error-boundary";
+import ErrorPage from "@/pages/ErrorPage";
+import LoadingFallback from "@/components/LoadingFallback";
 
 export const App = () => {
   return (
-    <QueryProvider>
-      <AuthProvider>
-        {/* TODO: Should Add better UI for loading state */}
-        <Suspense fallback={<div className="app-loading">Loading...</div>}>
-          <Router />
-        </Suspense>
-      </AuthProvider>
-    </QueryProvider>
+    <ErrorBoundary FallbackComponent={ErrorPage}>
+      <QueryProvider>
+        <AuthProvider>
+          <Suspense fallback={<LoadingFallback />}>
+            <Router />
+          </Suspense>
+        </AuthProvider>
+      </QueryProvider>
+    </ErrorBoundary>
   );
 };
